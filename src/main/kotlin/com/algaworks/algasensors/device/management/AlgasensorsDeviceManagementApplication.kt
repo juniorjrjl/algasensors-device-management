0fd016e8486b07866b9c1f2d9ca1c836367e3103
Junior <junior.jr.jl@gmail.com>
2025-05-29 19:00:34 -0300

    feat: create spring project

---
+++ src/main/kotlin/com/algaworks/algasensors/device/management/AlgasensorsDeviceManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.algaworks.algasensors.device.management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgasensorsDeviceManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlgasensorsDeviceManagementApplication>(*args)
+}
