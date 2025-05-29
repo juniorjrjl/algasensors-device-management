882cca85c5a62f50c9646022991e296e803f5e68
Junior <junior.jr.jl@gmail.com>
2025-05-29 19:20:03 -0300

    fix: replace kotlin to java

---
+++ src/main/java/com/algaworks/algasensors/device/management/AlgasensorsDeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.device.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgasensorsDeviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgasensorsDeviceManagementApplication.class, args);
+	}
+
+}
