06c3548d58fa830ebb71cb0756b844037f54ca85
Junior <junior.jr.jl@gmail.com>
2025-06-19 20:53:16 -0300

    feat: estudo de tratamento de erros

--- src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -1,10 +1,15 @@
 package com.algaworks.algasensors.device.management.api.client.impl;
 
 import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClient;
+import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
@@ -12,7 +17,20 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
     private final RestClient restClient;
 
     public SensorMonitoringClientImpl(final RestClient.Builder builder) {
-        this.restClient = builder.baseUrl("http://localhost:8082").build();
+        this.restClient = builder.baseUrl("http://localhost:8082")
+                .requestFactory(generateClientHttpRequestFactory())
+                .defaultStatusHandler(HttpStatusCode::isError,
+                        (req, res) -> {
+                    throw new SensorMonitoringClientBadGatewayException();
+                })
+                .build();
+    }
+
+    private static ClientHttpRequestFactory generateClientHttpRequestFactory(){
+        var factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(Duration.ofSeconds(5));
+        factory.setConnectTimeout(Duration.ofSeconds(5));
+        return factory;
     }
 
     @Override
