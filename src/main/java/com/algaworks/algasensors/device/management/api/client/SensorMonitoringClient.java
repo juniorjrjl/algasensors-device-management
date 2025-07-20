515a2f19b71b99a9599ef10fa22861069df9a934
Junior <junior.jr.jl@gmail.com>
2025-07-20 13:44:14 -0300

    feat: improving restClient

--- src/main/java/com/algaworks/algasensors/device/management/api/client/SensorMonitoringClient.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/SensorMonitoringClient.java
@@ -2,11 +2,22 @@ package com.algaworks.algasensors.device.management.api.client;
 
 import com.algaworks.algasensors.device.management.api.model.SensorMonitoringDetailResponse;
 import io.hypersistence.tsid.TSID;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PutExchange;
 
+@HttpExchange("/api/sensors/{sensorId}")
 public interface SensorMonitoringClient {
 
-    void enableMonitoring(final TSID id);
-    void disableMonitoring(final TSID id);
-    SensorMonitoringDetailResponse getDetail(TSID sensorId);
+    @PutExchange("/enable")
+    void enableMonitoring(@PathVariable final TSID sensorId);
+
+    @DeleteExchange("/enable")
+    void disableMonitoring(@PathVariable final TSID sensorId);
+
+    @GetExchange
+    SensorMonitoringDetailResponse getDetail(@PathVariable final TSID sensorId);
 
 }
